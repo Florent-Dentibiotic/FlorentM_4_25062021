19cb0151c8e9c88e14e7d849d0a7e7680528a831
MacbookAir <fmdentiste@pm.me>
2021-07-02 17:09:11 +0200

    Regex are in da place

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,6 +20,12 @@ let birthdate = document.getElementById('birthdate');
 let gamesQuantity = document.getElementById('quantity');
 let location1 = document.querySelectorAll(".cities");
 let useConditions = document.getElementById('checkbox1');
+let form_OK = true;
+const regexFirst = /^[a-zA-Z][a-zA-Z]+$/;
+const regexLast = /^[a-zA-Z][a-zA-Z'-]+$/;
+const regexEmail = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;
+const regexBirthdate = /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/;
+const regexGamesQuantity = /^[0-99]$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -37,45 +43,63 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-firstName.addEventListener('change', function(event) {
-  if (event.target.validity.valid) {
-    firstName.parentElement.lastElementChild.classList.replace("d-block", "d-none");
-  } else {
+//Event listerner during field completion :
+firstName.addEventListener('change', testRegexFirst, false);
+lastName.addEventListener('change', testRegexLast, false);
+email.addEventListener('change', testRegexEmail(), false);
+birthdate.addEventListener('change', testRegexBirthdate(), false);
+gamesQuantity.addEventListener('change', testRegexGamesQuantity(), false);
+
+useConditions.addEventListener('change', useConditionTest, false)
+
+// Regex tests :
+function testRegexFirst () {
+  if (regexFirst.test(firstName.value) == false) {
     firstName.parentElement.lastElementChild.classList.replace("d-none", "d-block");
-  }
-}, false);
-
-lastName.addEventListener('change', function(event) {
-  if (event.target.validity.valid) {
-    lastName.parentElement.lastElementChild.classList.replace("d-block", "d-none");
+    firstName.classList.remove("valid-field");
   } else {
-    lastName.parentElement.lastElementChild.classList.replace("d-none", "d-block");
+    firstName.parentElement.lastElementChild.classList.replace("d-block", "d-none");
+    firstName.classList.add("valid-field");
   }
-}, false);
+};
 
-email.addEventListener('change', function(event) {
-  if (event.target.validity.valid) {
+function testRegexLast () {
+  if (regexLast.test(lastName.value) == false) {
+    lastName.parentElement.lastElementChild.classList.replace("d-none", "d-block");
+    lastName.classList.remove("valid-field");
+  } else {
+    lastName.parentElement.lastElementChild.classList.replace("d-block", "d-none");
+    lastName.classList.add("valid-field");
+  }
+};
+
+function testRegexEmail() {
+  if (regexEmail.test(email.value) == true) {
     email.parentElement.lastElementChild.classList.replace("d-block", "d-none");
+    email.classList.add("valid-field");
   } else {
     email.parentElement.lastElementChild.classList.replace("d-none", "d-block");
+    email.classList.remove("valid-field");
   }
-}, false);
+};
 
-birthdate.addEventListener('change', function(event) {
-  if (event.target.validity.valid) {
+function testRegexBirthdate() {
+  if (regexBirthdate.test(birthdate.value) == true) {
     birthdate.parentElement.lastElementChild.classList.replace("d-block", "d-none");
+    birthdate.classList.add("valid-field");
   } else {
     birthdate.parentElement.lastElementChild.classList.replace("d-none", "d-block");
+    birthdate.classList.remove("valid-field");
   }
-}, false);
+};
 
-gamesQuantity.addEventListener('change', function(event) {
-  if (event.target.validity.valid) {
+function testRegexGamesQuantity() {
+  if (regexGamesQuantity.test(gamesQuantity.value) == true) {
     gamesQuantity.parentElement.lastElementChild.classList.replace("d-block", "d-none");
   } else {
     gamesQuantity.parentElement.lastElementChild.classList.replace("d-none", "d-block");
   }
-}, false);
+};
 
 location1.forEach(element => element.addEventListener('change', function(event) {
   if (event.target.validity.valid) {
@@ -85,58 +109,29 @@ location1.forEach(element => element.addEventListener('change', function(event) 
   }
 }, false));
 
-useConditions.addEventListener('change', function(event) {
-  if (event.target.validity.valid) {
-    useConditions.parentElement.lastElementChild.classList.replace("d-block", "d-none");
+function useConditionTest() {
+  if (useConditions.checked == false){
+    useConditions.nextElementSibling.nextElementSibling.nextElementSibling.classList.replace("d-none", "d-block");
   } else {
-    useConditions.parentElement.lastElementChild.classList.replace("d-none", "d-block");
-  }
-}, false);
+    useConditions.nextElementSibling.nextElementSibling.nextElementSibling.classList.replace("d-block", "d-none");
+  } 
+};
 
 // controle de remplissage des formulaires
 function testFieldsValidity(){
-  if (firstName.validity.valid == false){
-  firstName.parentElement.lastElementChild.classList.replace("d-none", "d-block");
-  } else {
-    firstName.parentElement.lastElementChild.classList.replace("d-block", "d-none");
-  } 
+  testRegexFirst ();
+  testRegexLast ();
+  testRegexEmail();
+  testRegexBirthdate();
+  testRegexGamesQuantity();
 
-  if (lastName.validity.valid == false){
-    lastName.parentElement.lastElementChild.classList.replace("d-none", "d-block");
-  } else {
-      lastName.parentElement.lastElementChild.classList.replace("d-block", "d-none");
-  } 
-
-  if (email.validity.valid == false){
-    email.parentElement.lastElementChild.classList.replace("d-none", "d-block");
-  } else {
-      email.parentElement.lastElementChild.classList.replace("d-block", "d-none");
-  } 
-
-  if (birthdate.validity.valid == false){
-    birthdate.parentElement.lastElementChild.classList.replace("d-none", "d-block");
-  } else {
-      birthdate.parentElement.lastElementChild.classList.replace("d-block", "d-none");
-  } 
-
-  if (gamesQuantity.validity.valid == false){
-    gamesQuantity.parentElement.lastElementChild.classList.replace("d-none", "d-block");
-  } else {
-      gamesQuantity.parentElement.lastElementChild.classList.replace("d-block", "d-none");
-  } 
+  useConditionTest()
 
   if (location1[0].validity.valid == false){
     location1[0].parentElement.lastElementChild.classList.replace("d-none", "d-block");
   } else {
     location1[0].parentElement.lastElementChild.classList.replace("d-block", "d-none");
   } 
-
-  if (useConditions.validity.valid == false){
-    useConditions.nextElementSibling.nextElementSibling.nextElementSibling.classList.replace("d-none", "d-block");
-  } else {
-      useConditions.nextElementSibling.nextElementSibling.nextElementSibling.classList.replace("d-block", "d-none");
-  } 
-
 };
 
 sumitInscription.addEventListener("click", testFieldsValidity);
